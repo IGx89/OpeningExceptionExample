b261804752f1c378c6e3ecf1fe2d581458499812
Matthew Lieder <MLieder@lt.life>
2019-03-01 17:45:22 -0600

    Initial commit

---
+++ OpeningExceptionExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using ServiceReference1;
+using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace OpeningExceptionExample
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Calling SOAP service...");
+
+            var client = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+
+            ((ICommunicationObject)client).Opening += (sender, a) => throw new Exception("Triggering Faulted state");
+
+            await client.AddAsync(1, 1);
+
+            Console.WriteLine("Succeeded");
+        }
+    }
+}
